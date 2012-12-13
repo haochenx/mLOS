d80c46acf1641bdd0a69ccc121de32711d61e9db
Haochen Xie <Max.T.Xie@gmail.com>
2012-12-13 00:10:36 -0800

    Push codes up to now

---
+++ class.lua
@@ -0,0 +1,125 @@
+-- This file tries to implement a class system in lua
+
+function classify(ct)
+   local fields = {}
+   local methods = {}
+
+   -- Populate <fields> & <methods>
+   for k, v in pairs(ct) do
+      local ignore = {
+	 ancestors=true,
+	 constructor=true,
+	 __ht=true
+      }
+      if not ignore[k] then
+	 if type(v) == "function" then
+	    methods[k] = v
+	 else
+	    fields[k] =v
+	 end
+      end
+   end
+
+   -- create a hidden table to keep helper
+   ct.__ht = {
+      fields=fields,
+      methods=methods
+   }
+   local constructor_mt = {}
+   local instance_mt = {
+      __index = function(self, k) return ct.__ht.selectMethod(k) end
+   }
+
+   -- Helper function <selectMethod>
+   function ct.__ht.selectMethod(k)
+      -- Try to select method
+      if ct.__ht.methods[k] then
+	 return ct.__ht.methods[k]
+      end
+
+      -- Try ancestors' method
+      if ct.ancestors then
+	 for _, v in pairs(ct.ancestors) do
+	    if v.__ht.methods[k] then
+	       return v.__ht.methods[k]
+	    end
+	 end
+      end
+      return nil
+   end
+   -- Helper function <copy>
+   function ct.__ht.copy(t)
+      nt = {}
+      for k, v in pairs(t) do
+	 nt[k] = v
+      end
+      return nt
+   end
+   -- Helper function <concat_table>
+   function ct.__ht.concat_table(a, b)
+      for k, v in pairs(b) do
+	 a[k] = v
+      end
+   end
+
+   function constructor_mt.__call(self,...)
+      -- Load initial fields
+      local new_instance = ct.__ht.copy(fields)
+
+      -- Load ancestors' initial fields
+      if ct.ancestors then
+	 for k, v in pairs(ct.ancestors) do
+	    ct.__ht.concat_table(new_instance, v.__ht.fields)
+	 end
+      end
+
+      if ct.constructor then
+	 ct.constructor(new_instance,...)
+      end
+
+      setmetatable(new_instance, instance_mt)
+      return new_instance
+   end
+
+   -- Check if <x> is instance according to matching of fields
+   function ct.__ht.is_instance_fields(x)
+      -- Check for itself
+      for k in pairs(ct.__ht.fields) do
+	 if not x[k] then return false end
+      end
+      -- Check for it's ancesors
+      if ct.ancestors then
+	 for _, v in pairs(ct.ancestors) do
+	    if not v.__ht.is_instance_fields(x) then return false end
+	 end
+      end
+      return true
+   end
+   function ct.__ht.method_list()
+      local method_list = {}
+      for k in pairs(ct.__ht.methods) do
+	 method_list[#method_list] = k
+      end
+      if ct.ancestors then
+	 for _, v in pairs(ct.ancestors) do
+	    ct.__ht.concat_table(method_list, v.__ht.method_list())
+	 end
+      end
+      return method_list
+   end
+
+   function ct.is_instance(x)
+      -- Check field matching
+      if not ct.__ht.is_instance_fields(x) then return false end
+
+      -- Check method matching
+      local method_list = ct.__ht.method_list()
+      for i = 1, #method_list do
+	 if ct.__ht.selectMethod(method_list[i]) ~= x.method_list[i] then
+	    return false end
+      end
+      return true
+   end
+
+   setmetatable(ct, constructor_mt)
+end
---
+++ test.lua
@@ -0,0 +1,73 @@
+Point = {
+   x=0,
+   y=0,
+   distSq
+      = function(self)
+	   local function sq(x) return x*x end
+	   return sq(self.x) + sq(self.y)
+	end,
+   print
+      = function(self)
+	   print(self.x, self.y)
+	end,
+   constructor
+      = function(self, x, y)
+	   self.x = x or self.x
+	   self.y = y or self.y
+	end
+}
+
+Point3d = {
+   ancestors = {Point},
+   z=0,
+   distSq
+      = function(self)
+	   local function sq(x) return x*x end
+	   return sq(self.x) + sq(self.y) + sq(self.z)
+	end,
+   constructor
+      = function(self, x, y, z)
+	   self.x = x or self.x
+	   self.y = y or self.y
+	   self.z = z or self.z
+	end
+}
+
+classify(Point)
+classify(Point3d)
+
+function dump_table(t) table.foreach(t, print) end
+
+print("\nPoint:")
+dump_table(Point)
+print("\nPoint3d:")
+dump_table(Point3d)
+
+p1 = Point()
+p2 = Point3d()
+
+print("\np1:")
+dump_table(p1)
+print("\np2:")
+dump_table(p2)
+
+p1 = Point(2, 4)
+p2 = Point3d(3, 4, 5)
+
+print("\np1:")
+dump_table(p1)
+print("\np2:")
+dump_table(p2)
+
+print("\np1:distSq()")
+print(p1:distSq())
+print("\np2:distSq()")
+print(p2:distSq())
+
+print()
+print(Point3d.is_instance(p1))
+print(Point3d.is_instance(p2))
+
+print()
+print(Point.is_instance(p1))
+print(Point.is_instance(p2))
